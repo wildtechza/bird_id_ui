1fc96adc7b01b245ae2717407a54903f7cb10322
Nicholas Cook <cooknh@gmail.com>
2025-09-20 17:42:01 +0200

    chore: force light theme

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -24,6 +24,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <meta name="theme-color" content="#ffffff" />
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
