2d30d0002573d456ea77c75f830ae73040305824
wildtechza <wildtechza@users.noreply.github.com>
2025-09-20 10:14:15 +0000

    deploy: 6ae30f3b673143a16470e93acb06d86288822fde

--- next.config.ts
+++
@@ -1,8 +0,0 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  reactStrictMode: true,
-  output: "export", // 👈 replaces `next export`
-};
-
-export default nextConfig;
--- src/models/Question.ts
+++
@@ -1,8 +0,0 @@
-export class Question {
-  image!: string;
-  answer!: string;
-
-  constructor(data: Partial<Question>) {
-    Object.assign(this, data);
-  }
-}
