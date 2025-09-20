babdde2047594ca2d55581f96e9f076873959960
Nicholas Cook <cooknh@gmail.com>
2025-09-20 11:53:01 +0200

    chore: new export style

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  reactStrictMode: true,
+  output: "export", // 👈 replaces `next export`
 };
 
 export default nextConfig;
